418e202a052cd4cf1ab81583a63d7cd1b7aa0a83
Muhammed <muhamme7d7@gmail.com>
2025-02-05 23:15:28 -0900

    fix

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    
+      <main className="">
+       <h1>lets build an API productivity APP</h1>
+
+        <Button>Click me</Button>
+      </main>
+      
+  );
+}
