1096523d47b3b711b0eeda7e597e7b92709340d9
Muhammed <muhamme7d7@gmail.com>
2025-02-09 22:56:39 -0900

    integrate Sidebar component into RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 import Header from "../components/Header";
+import Sidebar from "../components/ui/sidebar";
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +21,7 @@ export default function RootLayout({
         <body>
           <Header />
           <div className="flex min-h-screen">
-            {/*sidebar*/}
+            <Sidebar />
           
           <div className="flex-1 p-4 bg-gray-100 overflow-y-auto scrollbar-hide">{children}</div>
           </div>
